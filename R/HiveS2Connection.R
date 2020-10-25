c3de064bea436be621be09ad8a660833e284e909
Agnieszka Z <zuk.ness@gmail.com>
2020-10-25 23:29:53 +0100

    dbListTables empty list error fixed

--- R/HiveS2Connection.R
+++ R/HiveS2Connection.R
@@ -5,7 +5,36 @@ setClass("jdbcHiveDriver",
           contains = "DBIDriver",
          slots = c(identifier.quote="character",
                    jdrv="jobjRef"))
+
+#' method "dbConnect" copied from RJDBC with JDBCConnection replaced with jdbcHiveDriver
+#'
+#' @export
+setMethod("dbConnect", "jdbcHiveDriver",
+  function(
+    drv,
+    url,
+    schema=NULL,
+    user='',
+    password='',
+    ...
+    ) {
+  jc <- .jcall("java/sql/DriverManager","Ljava/sql/Connection;","getConnection", as.character(paste(url,"/",schema,sep = ""))[1], as.character(user)[1], as.character(password)[1], check=FALSE)
+  if (is.jnull(jc) && !is.jnull(drv@jdrv)) {
+    oex <- .jgetEx(TRUE)
+    p <- .jnew("java/util/Properties")
+    if (length(user)==1 && nzchar(user)) .jcall(p, "Ljava/lang/Object;", "setProperty", "user", as.character(user))
+    if (length(password)==1 && nzchar(password)) .jcall(p, "Ljava/lang/Object;", "setProperty", "password", as.character(password))
+    l <- list(...)
+    if (length(names(l))) for (n in names(l)) .jcall(p, "Ljava/lang/Object;", "setProperty", n, as.character(l[[n]]))
+    jc <- .jcall(drv@jdrv, "Ljava/sql/Connection;", "connect", as.character(as.character(paste(url,"/",schema,sep = "")))[1], p, check=FALSE)
+    print(as.character(paste(url,"/",schema,sep = "")))
+  }
+  .verify.JDBC.result(jc, "Unable to connect JDBC to ",url)
+  new("HS2Connection", jc=jc, identifier.quote=drv@identifier.quote)},
+  valueClass="HS2Connection")
+
 #' function HiveS2 copied from RJDBC
+#' create jdbcHiveDriver object
 #' @export
 HiveS2 <- function(driverClass='', classPath='', identifier.quote=NA) {
   classPath <- path.expand(unlist(strsplit(classPath, .Platform$path.sep)))
@@ -29,23 +58,6 @@ HiveS2 <- function(driverClass='', classPath='', identifier.quote=NA) {
   }
 }
 
-#' method "dbConnect" copied from RJDBC with JDBCConnection replaced with jdbcHiveDriver
-#'
-#' @export
-setMethod("dbConnect", "jdbcHiveDriver", def=function(drv, url, user='', password='', ...) {
-  jc <- .jcall("java/sql/DriverManager","Ljava/sql/Connection;","getConnection", as.character(url)[1], as.character(user)[1], as.character(password)[1], check=FALSE)
-  if (is.jnull(jc) && !is.jnull(drv@jdrv)) {
-    oex <- .jgetEx(TRUE)
-    p <- .jnew("java/util/Properties")
-    if (length(user)==1 && nzchar(user)) .jcall(p, "Ljava/lang/Object;", "setProperty", "user", as.character(user))
-    if (length(password)==1 && nzchar(password)) .jcall(p, "Ljava/lang/Object;", "setProperty", "password", as.character(password))
-    l <- list(...)
-    if (length(names(l))) for (n in names(l)) .jcall(p, "Ljava/lang/Object;", "setProperty", n, as.character(l[[n]]))
-    jc <- .jcall(drv@jdrv, "Ljava/sql/Connection;", "connect", as.character(url)[1], p, check=FALSE)
-  }
-  .verify.JDBC.result(jc, "Unable to connect JDBC to ",url)
-  new("HS2Connection", jc=jc, identifier.quote=drv@identifier.quote)},
-  valueClass="HS2Connection")
 
 #' HS2Connection class inherits from JDBCConnection (RJDBC)
 #' @rdname HS2Connection-class
@@ -70,15 +82,6 @@ setMethod("dbQuoteIdentifier", c("HS2Connection", "character"), function(conn, x
 #' @export
 setMethod("dbQuoteIdentifier", c("HS2Connection", "SQL"), function(conn, x, ...) {
   x
-})
-
-
-#' TODO: why dbListTables shows character(0)
-#' @export
-setMethod("dbListTables", "HS2Connection", function(conn, pattern="%", schema=NULL, ...) {
-  d <- dbGetQuery(conn, "show tables")
-  tnames <- d["tableName"]
-  as.vector(t(tnames))
 })
 
 
---
+++ R/dbListTables.R
@@ -0,0 +1,20 @@
+
+#' TODO: why dbListTables shows character(0)
+#' when specifying schema all queries that follow will be executed on that schema (warning message)
+#' @export
+setMethod("dbListTables", "HS2Connection", function(conn, pattern=NULL, schema=NULL, ...) {
+  if (!is.null(pattern)) {
+    statement <- paste('SHOW TABLES LIKE', dbQuoteString(conn, pattern))
+  }
+  if(!(is.null(schema))){
+    d <- dbGetQuery(conn, paste("use ",schema))
+    warning(paste("switched to a  schema: ",schema))
+  }
+  d <- dbGetQuery(conn, "show tables")
+  tnames <- d["tableName"]
+  if(length(t(tnames))){
+  as.vector(t(tnames))
+  } else {
+    character(0)
+  }
+})
