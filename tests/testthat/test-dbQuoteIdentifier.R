4c507f8051a2f8505ea8e5b9fdc885ab3b4f0de9
Agnieszka Z <zuk.ness@gmail.com>
2020-12-27 00:47:04 +0100

    Update test-dbQuoteIdentifier.R

--- tests/testthat/test-dbQuoteIdentifier.R
+++ tests/testthat/test-dbQuoteIdentifier.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+source('utilities.R')
+
+conn <- HiveS2_TestConnection()
+
+test_that("quote identifier", {
+    expect_equal(dbQuoteIdentifier(conn, character()), SQL(character()))
+    expect_equal(dbQuoteIdentifier(conn, "a"), SQL('`a`'))
+    expect_equal(dbQuoteIdentifier(conn, "a b"), SQL('`a b`'))
+
+    expect_equal(dbQuoteIdentifier(conn, SQL('"a"')), SQL('"a"'))
+    expect_equal(dbQuoteIdentifier(conn, SQL('"a b"')), SQL('"a b"'))
+  })
+
