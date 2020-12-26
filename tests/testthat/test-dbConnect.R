5ac8119e611e1cb0fdea51525d6bcd8f27e1c5c5
Agnieszka Z <zuk.ness@gmail.com>
2020-12-26 22:06:04 +0100

    Update test-dbConnect.R

--- tests/testthat/test-dbConnect.R
+++ tests/testthat/test-dbConnect.R
@@ -1,12 +1,14 @@
 source('utilities.R')
+conn <- HiveS2_TestConnection()
+
+test_that("Test Connection is not null", {
+  expect_true(!is.null(conn))
+})
 test_that("querying closed connection throws error", {
-  conn <- HiveS2_TestConnection()
   dbDisconnect(conn)
   expect_error(
     dbGetQuery(conn, "select * from foo"),
     "java.sql.SQLException: Can't create Statement, connection is closed",
     fixed = TRUE
   )
-  conn <- HiveS2_TestConnection()
-
 })
