7bcf60b83429416359766a1644eab5bd575e0f85
Agnieszka Z <zuk.ness@gmail.com>
2020-12-27 00:08:01 +0100

    Update test-dbListTables.R

--- tests/testthat/test-dbListTables.R
+++ tests/testthat/test-dbListTables.R
@@ -1,3 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+source('utilities.R')
+
+conn <- HiveS2_TestConnection()
+
+test_that('dbListTables returns character(0) when empty database', {
+  expect_equal(length(dbListTables(conn)), 0)
 })
+
+test_that('dbListTables returns character(0) when no match found', {
+  expect_equal(
+    dbListTables(conn, pattern='no_match'),
+    character(0)
+  )
+})
+
+test_that('dbListTables returns list of tables', {
+  expect_gt(length(dbListTables(conn, schema = "so_survey_2019_hive")), 0)
+})
+
+dbDisconnect(conn)
