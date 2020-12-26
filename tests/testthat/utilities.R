56a842fddae37fc72a84e661323056317b172603
Agnieszka Z <zuk.ness@gmail.com>
2020-12-26 22:06:49 +0100

    schema default

--- tests/testthat/utilities.R
+++ tests/testthat/utilities.R
@@ -7,7 +7,7 @@ HiveS2_TestConnection <- function(){
   conn <- DBI::dbConnect(HiveS2("org.apache.hive.jdbc.HiveDriver","/Users/zukow/spark-2.2.1-bin-hadoop2.7/jars/hive-jdbc-1.2.1.spark2.jar",identifier.quote='`'),
                          host ="jdbc:hive2://localhost:",
                          port = "10000",
-                         schema = "so_survey_2019_hive"
+                         schema = "default"
   )
 
 }
