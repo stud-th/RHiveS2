3134a8d98336bbbc53f6c1ccab772f32796297c5
Agnieszka Z <zuk.ness@gmail.com>
2020-12-29 17:46:01 +0100

    Create test-copy_to.R

---
+++ tests/testthat/test-copy_to.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(dbplyr)
+library(testthat)
+library(RHiveS2)
+library(RJDBC)
+source('/Users/zukow/Documents/GitHub/RHiveS2/tests/testthat/utilities.R')
+
+test_that("dbWriteTable not implemented yet", {
+  name = "iris"
+  data = iris
+  repartition = 0L
+  sc <- HiveS2_TestConnection()
+  expect_error(
+    tbl <- dplyr::copy_to(sc, data, name = name, repartition = repartition),
+    "dbWriteTable not implemented.",
+    fixed = TRUE
+  )
+})
+
+
+
+
+
+
