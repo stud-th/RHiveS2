d7fa7772b6f0f9645366bd0d1f7c81e7530e03ab
Agnieszka Z <zuk.ness@gmail.com>
2021-01-02 22:08:16 +0100

    Update test-dbListTables.R

--- tests/testthat/test-dbListTables.R
+++ tests/testthat/test-dbListTables.R
@@ -6,12 +6,14 @@ test_that('dbListTables returns character(0) when empty database', {
   expect_equal(length(dbListTables(conn)), 0)
 })
 
+dbSendQuery(conn, "create table foo (test int)")
 test_that('dbListTables returns character(0) when no match found', {
   expect_equal(
     dbListTables(conn, pattern='no_match'),
     character(0)
   )
 })
+dbSendQuery(conn, "drop table foo")
 
 test_that('dbListTables returns list of tables', {
   expect_warning(x<-length(dbListTables(conn, schema = "so_survey_2019_hive")))
