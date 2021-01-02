03a312599674f6f579df362a041588c2af648d75
Agnieszka Z <zuk.ness@gmail.com>
2021-01-02 19:55:05 +0100

    Create test-dbGetQuery.R

---
+++ tests/testthat/test-dbGetQuery.R
@@ -0,0 +1,9 @@
+source('utilities.R')
+
+conn <- HiveS2_TestConnection()
+
+test_that("dbGetQuery works with parameterized queries", {
+  boeing_param <- dbGetQuery(conn, "SELECT * FROM nycflights13.planes WHERE manufacturer = ?", "BOEING")
+  boeing <- dbGetQuery(conn, "select count(*) from (SELECT * FROM nycflights13.planes WHERE manufacturer = 'BOEING')")
+  expect_equal(nrow(boeing_param), boeing[1,1])
+})
