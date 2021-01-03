bbcc59b29cc37da60023df9700f4880f244f6d7c
Agnieszka Z <zuk.ness@gmail.com>
2021-01-03 03:54:44 +0100

    Auto commint disabled by dafault

--- R/dbi_transactions.R
+++ R/dbi_transactions.R
@@ -1,10 +1,9 @@
-#sparklyr/R/dbi_spark_transactions.R
 setMethod("dbBegin", "HiveS2Connection", function(conn) {
   TRUE
 })
 
 setMethod("dbCommit", "HiveS2Connection", function(conn) {
-  TRUE
+  FALSE
 })
 
 setMethod("dbRollback", "HiveS2Connection", function(conn) {
