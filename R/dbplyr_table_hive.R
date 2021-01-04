cb0cd6498d30cfe36348cecf41f8713a5486c801
Agnieszka Z <zuk.ness@gmail.com>
2021-01-04 02:20:15 +0100

    Update dbplyr_table_hive.R

--- R/dbplyr_table_hive.R
+++ R/dbplyr_table_hive.R
@@ -1,4 +1,3 @@
-#'@import dplyr db_save_query
 #' @export
 db_save_query.HiveS2Connection <- function(con, sql, name, temporary = TRUE, ...) {
   sql <- sql_query_save(con, sql, name, temporary = temporary, ...)
@@ -7,21 +6,18 @@ db_save_query.HiveS2Connection <- function(con, sql, name, temporary = TRUE, ...
 }
 
 sql_query_save<- function(con, sql, name, temporary = TRUE, ...) {
-  build_sql(
+  dbplyr::build_sql(
     "CREATE ", if (temporary) sql("TEMPORARY "), "VIEW \n",
-    as.sql(name), " AS ", sql,
+    dbplyr::as.sql(name), " AS ", sql,
     con = con
   )
 }
 
 #' @export
-#' @importFrom dplyr db_analyze
-#' @importFrom dbplyr build_sql
-#' @importFrom DBI dbExecute
 db_analyze.HiveS2Connection <- function(con, table, ...) {
-  info <- dbGetQuery(con, build_sql("SHOW TABLES LIKE ", dbQuoteString(con, table), con = con))
+  info <- dbGetQuery(con, dbplyr::build_sql("SHOW TABLES LIKE ", dbQuoteString(con, table), con = con))
   if (nrow(info) > 0 && identical(info$isTemporary, FALSE)) {
-    sql<- build_sql("ANALYZE TABLE", as.sql(table), "COMPUTE STATISTICS", con = con)
+    sql<- dbplyr::build_sql("ANALYZE TABLE", dbplyr::as.sql(table), "COMPUTE STATISTICS", con = con)
     dbExecute(con, sql)
   }
 }
