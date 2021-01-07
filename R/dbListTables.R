5488fa35a680c5e96e1151a43849b249ad92e7e5
Agnieszka Z <zuk.ness@gmail.com>
2021-01-07 05:07:53 +0100

    unquote sql

--- R/dbListTables.R
+++ R/dbListTables.R
@@ -1,8 +1,12 @@
 #' when specifying schema all queries that follow will be executed on that schema (warning message)
 #' @include HiveS2Connection.R
+#' @rdname HiveS2Connection-class
+#' @param pattern - specify pattern for SQL 'SHOW TABLES LIKE' statment
+#' @param schema - database which tables will be listed from
 #' @export
 setMethod("dbListTables", "HiveS2Connection", function(conn, pattern=NULL, schema=NULL, ...) {
   if (!is.null(pattern)) {
+    if(!is.character(pattern))noquote(pattern)
     statement <- paste('SHOW TABLES LIKE', dbQuoteString(conn, paste(pattern)))
   }
   else statement <- "show tables"
