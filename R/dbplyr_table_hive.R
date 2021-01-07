4c9dba54276969ef20fcbb8de602be7416fff1e0
Agnieszka Z <zuk.ness@gmail.com>
2021-01-07 05:08:48 +0100

    custom scalar and string functions

--- R/dbplyr_table_hive.R
+++ R/dbplyr_table_hive.R
@@ -21,3 +21,41 @@ db_analyze.HiveS2Connection <- function(con, table, ...) {
     dbExecute(con, sql)
   }
 }
+
+#' @export
+db_drop_table.HiveS2Connection <- function(con, table, ...) {
+  sql <- dbplyr::build_sql(
+    "DROP TABLE ", sql("IF EXISTS "), dbplyr::as.sql(table),
+    con = con
+  )
+  dbSendQuery(con, sql)
+}
+
+
+#adopted from  dbplyr version 2.0.0
+#' @importFrom dbplyr sql_variant sql_translator sql_prefix base_scalar base_agg base_win sql_aggregate win_aggregate sql_expr
+#' @export
+sql_translate_env.HiveS2Connection <- function(con) {
+  sql_variant(
+
+    sql_translator(.parent = base_scalar,
+                   bitwShiftL    = sql_prefix("SHIFTLEFT", 2),
+                   bitwShiftR    = sql_prefix("SHIFTRIGHT", 2),
+
+                   cot = function(x){
+                     sql_expr(1 / tan(!!x))
+                   },
+
+                   str_replace_all = function(string, pattern, replacement) {
+                     sql_expr(regexp_replace(!!string, !!pattern, !!replacement))
+                   }
+    ),
+    sql_translator(.parent = base_agg,
+                   n  = function() sql("COUNT(*)"), # issue in dbplyr 1.4.4  #343
+                   var = sql_aggregate("VARIANCE", "var"),
+    ),
+    sql_translator(.parent = base_win,
+                   var = win_aggregate("VARIANCE"),
+    )
+  )
+}
