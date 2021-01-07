feaf38693c3ac1f3307706f33e7efd52b5e54af5
Agnieszka Z <zuk.ness@gmail.com>
2021-01-07 05:07:19 +0100

    unsupported complex types

--- R/dbDataType.R
+++ R/dbDataType.R
@@ -21,29 +21,6 @@ data_type <- function(dbObj,x) {
     # #TODO: map and array needs further implementation
     #list = list_to_map_array,
     stop("Unsupported type: ", typeof(x), call. = FALSE)
-  )
-}
-
-# list_to_map_array <- function(obj){
-#   if (data.class(obj) == 'list') {
-#     if (length(obj) == 0) {
-#       inner.type <- data_type(dbObj, NULL)
-#     } else {
-#       inner.types <- vapply(obj, function(x) data_type(dbObj, x), '')
-#       inner.type <- inner.types[1]
-#       if (!all(inner.types == inner.type)) {
-#         inner.type <- NA
-#       }
-#     }
-#     if (is.na(inner.type)) {
-#       rv <- 'varchar'
-#     } else {
-#       if (!is.null(names(obj))) {
-#         rv <- paste('map<varchar, ', inner.type, '>', sep='')
-#       } else {
-#         rv <- paste('array<', inner.type, '>', sep='')
-#       }
-#     }
-#   }
-#   return(stringi::stri_trans_toupper(rv, 'en_US.UTF-8'))
-# }
+# unsupported conversion to Hive Complex Types
+# ARRAY<data_type>,  MAP<primitive_type, data_type>, STRUCT...
+  )}
